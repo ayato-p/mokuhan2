e8d2c09747bd820ba1417986252017c5da89df88
ayato_p <ayato.skycrawlers@gmail.com>
2019-12-26 15:21:30 +0900

    Initial commit :)

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.panchromatic/tiny-mokuhan "0.1.0-SNAPSHOT"
+  :description "Tiny Implementation of Mokuhan"
+  :url "https://github.com/ayato-p/mokuhan"
+  :license {:name "MIT License"
+            :url "https://choosealicense.com/licenses/mit"}
+  :dependencies []
+  :profiles
+  {:provided
+   {:dependencies [[org.clojure/clojure "1.10.1"]]}})
