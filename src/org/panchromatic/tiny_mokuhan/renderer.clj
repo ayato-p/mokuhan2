d6a5964bed6c7ca7bb3f4c92ea98e86bc1477a34
ayato_p <ayato.skycrawlers@gmail.com>
2019-12-28 18:06:25 +0900

    Variable renderer

--- src/org/panchromatic/tiny_mokuhan/renderer.clj
+++ src/org/panchromatic/tiny_mokuhan/renderer.clj
@@ -1,26 +1,40 @@
 (ns org.panchromatic.tiny-mokuhan.renderer
-  (:import [org.panchromatic.tiny_mokuhan.ast Mustache Text NewLine Variable]))
+  (:require [clojure.string :as str])
+  (:import [org.panchromatic.tiny_mokuhan.ast Mustache NewLine Text Variable]))
 
 (defprotocol Renderer
-  (-render [renderable]))
+  (-render [renderable data]))
 
 (extend-type Mustache
   Renderer
-  (-render [mustache]
+  (-render [mustache data]
     (.toString
-     (reduce (fn [sb c] (.append sb (-render c)))
+     (reduce (fn [sb c] (.append sb (-render c data)))
              (StringBuilder.)
              (.contents mustache)))))
 
 (extend-type Text
   Renderer
-  (-render [text]
+  (-render [text _]
     (.content text)))
 
 (extend-type NewLine
   Renderer
-  (-render [_]
+  (-render [_ _]
     (System/lineSeparator)))
 
-(defn render [ast]
-  (-render ast))
+(defn- escape-html [s]
+  (-> s
+      (str/replace #"&" "&amp;")
+      (str/replace #"<" "&lt;")
+      (str/replace #">" "&gt;")
+      (str/replace #"\"" "&quot;")
+      (str/replace #"'" "&#39;")))
+
+(extend-type Variable
+  Renderer
+  (-render [variable data]
+    (escape-html (get data (keyword (.keys variable))))))
+
+(defn render [ast data]
+  (-render ast data))
--- test/org/panchromatic/tiny_mokuhan/renderer_test.clj
+++ test/org/panchromatic/tiny_mokuhan/renderer_test.clj
@@ -6,15 +6,25 @@
 (t/deftest render-text-test
   (let [ast (ast/mustache
              [(ast/text "Hello, world")])]
-    (t/is (= "Hello, world"
-             (sut/render ast))))
+    (t/is (= "Hello, world" (sut/render ast {}))))
 
   (let [ast (ast/mustache
              [(ast/text "Hello")
               (ast/newline)
               (ast/text "world")])]
-    (t/is (= "Hello\nworld"
-             (sut/render ast)))))
+    (t/is (= "Hello\nworld" (sut/render ast {})))))
 
 (t/deftest render-variable-test
-  )
+  (t/testing "simple interpolation"
+    (let [ast (ast/mustache
+               [(ast/text "Hello, ")
+                (ast/variable "name" {:open "{{" :close "}}"})
+                (ast/text ".")])]
+      (t/is (= "Hello, Taro."
+               (sut/render ast {:name "Taro"})))))
+
+  (t/testing "should escape"
+    (let [ast (ast/mustache
+               [(ast/variable "x" {:open "{{" :close "}}"})])]
+      (t/is (= "&lt;&#39;name&#39;&gt;"
+               (sut/render ast {:x "<'name'>"}))))))
