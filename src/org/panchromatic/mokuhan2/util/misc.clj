21c1b9b4006b0001c3db983cc7e49083482417a1
ayato-p <ayato-p@panchromatic.org>
2020-01-19 16:55:34 +0900

    Implements FixedLengthVector

--- src/org/panchromatic/mokuhan2/util/misc.clj
+++ src/org/panchromatic/mokuhan2/util/misc.clj
@@ -10,3 +10,23 @@
 
     :else
     m2))
+
+(deftype FixedLengthVector [n v]
+  clojure.lang.IPersistentCollection
+  (count [_]
+    n)
+  (cons [_ x]
+    (FixedLengthVector. n (subvec (conj v x) 1)))
+  (empty [_]
+    false)
+  (equiv [_ x]
+    (and (instance? FixedLengthVector x)
+         (= n (.n x))
+         (= v (.v x)))))
+
+(defn fixed-legnth-vector
+  ([n]
+   (fixed-legnth-vector n nil))
+  ([n init-val]
+   {:pre [(pos-int? n)]}
+   (FixedLengthVector. n (vec (repeat n init-val)))))
--- test/org/panchromatic/mokuhan2/util/misc_test.clj
+++ test/org/panchromatic/mokuhan2/util/misc_test.clj
@@ -23,3 +23,24 @@
 
   (t/is (= {:x {:y 2}}
            (sut/deep-merge {:x {:y 1}} {:x {:y 2}}))))
+
+(t/deftest fixed-legnth-vector-test
+  (t/testing "constructor"
+    (t/is (= [nil]
+             (.v (sut/fixed-legnth-vector 1))))
+
+    (t/is (= [false false]
+             (.v (sut/fixed-legnth-vector 2 false))))
+
+    (t/is (thrown? java.lang.AssertionError (.v (sut/fixed-legnth-vector 0)))))
+
+  (t/testing "conjoin"
+    (t/is (= [false true]
+             (-> (sut/fixed-legnth-vector 2 false)
+                 (conj true)
+                 (.v))))
+
+    (t/is (= [true]
+             (-> (sut/fixed-legnth-vector 1 false)
+                 (conj true)
+                 (.v))))))
